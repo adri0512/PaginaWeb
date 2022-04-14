32b258132bb2e4ec4643b61e537a0a820f69f2eb
practica <adriz.vallent@gmail.com>
2022-04-13 22:39:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.wrap', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
